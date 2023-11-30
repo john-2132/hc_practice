63806b31eb06b629e7b2ddc0efb8b76bfbe551bd
john-2132 <junichi.k0211@gmail.com>
2023-11-30 20:50:14 +0900

    A〜Fの6人グループをランダムに2−4、3−3に分ける処理

--- ruby/random.rb
+++ ruby/random.rb
@@ -1,1 +1,8 @@
-# ruby random
+# グループのメンバー
+members = ['A', 'B', 'C', 'D', 'E', 'F']
+
+# 第一グループをランダムに２〜４人で作る
+first_group = members.sample(rand(2..4)).sort
+
+# 第一グループ、第二グループ（全体から第一グループを抜く）を出力
+p first_group, members - first_group
